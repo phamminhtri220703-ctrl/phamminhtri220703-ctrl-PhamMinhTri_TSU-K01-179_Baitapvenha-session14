1fcb9d835a64668f265ef99ba4d03b9474ad45c3
phamminhtri220703-ctrl <phamminhtri220703@gmail.com>
2026-06-02 11:09:10 +0900

    Module01_session14_Ex01

---
+++ Module01_session14_Ex01/Module01_session14_Ex01.js
@@ -0,0 +1,9 @@
+const information = {
+    name: 'John Doe',
+    age: 25,
+    job: 'Developer',
+};
+
+console.log(`Họ và tên: ${information.name}`);
+console.log(`Tuổi: ${information.age}`);
+console.log(`Nghề nghiệp: ${information.job}`);
